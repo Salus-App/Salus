8459f10f6bc025c9cfeec3b719c915895b9da564
phoeN24 <phoenix@kilodelta.com>
2022-01-29 15:07:17 -0800

    apiRouter complete

--- server/router/api.js
+++ server/router/api.js
@@ -3,11 +3,24 @@ const userController = require('../controller/userController')
 const express = require('express');
 const router = express.Router();
 
+
+//get user
 router.get('/',
   userController.getUser,
   (req,res) => res.status(200).json({})
 );
 
+//get music
+router.get('/api/music', 
+  userController.getMusic,
+ (req,res) => res.sendStatus(200).json({})
+)
+
+//get podcasts
+router.get('/api/podcats', 
+  userController.getMusic,
+  (req,res) => res.sendStatus(200).json({})
+)
 
 
 //export
