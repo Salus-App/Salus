6b418d985ab7e9cb6c2ec03a3ac03da64db04dce
phoeN24 <phoenix@kilodelta.com>
2022-01-29 16:31:00 -0800

    started on userController

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,11 +1,23 @@
+const { resolvePath } = require('react-router-dom');
 const db = require('../model/userModel');
 
 const userController = {};
 
 // //create user
-// userController.createUser = (req, res,next) => {
-
-// };
+ userController.createUser = (req, res,next) => {
+    
+    (err, user) => {
+        if(err) {
+            return next({
+                log:`userController.createUser: ERROR: ${typeof err === 'object' ? JSON.stringify(err) : err}`,
+                message: {err: 'Error occured in userController.createUser. Check server logs for more info'}
+            });
+        }
+        const parsed = JSON.parse(user);
+        res.locals.user = parsed.results
+        return next();
+    }
+ };
 
 //get user
 userController.getUser = (req, res, next) => {
